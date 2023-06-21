0ed705e2b4ecba0e103625c19b955c0c1c1b18c9
mario marquesto <mariomarquestso@gmail.com>
2023-06-21 14:19:49 -0300

    trabajando con google fonts

--- fe-ct-prepcourse-fs-main/M08 JavaScript Callback/homework.js
+++ fe-ct-prepcourse-fs-main/M08 JavaScript Callback/homework.js
@@ -5,6 +5,7 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   
 }
 
 function invocarCallback(cb) {
